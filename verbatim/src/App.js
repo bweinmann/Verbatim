64203224475a13048d6c1bf5f029f9e4d5654ad5
bweinmann <bsweinmann@gmail.com>
2022-07-14 11:49:55 -0400

    api call successful, random word generated on page load

--- verbatim/src/App.js
+++ verbatim/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'b1583183e4mshbac800953b86b23p16f609jsn7913d01b1356',
+    'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+  }
+};
 
 function App() {
+
+  const [words, setWords] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://random-words5.p.rapidapi.com/getRandom', options);
+      const random = await res.text();
+      setWords(random)
+    }
+    fetchData()
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {words}
     </div>
   );
 }
