d45209ff8cffe2d4a29effafcb4c9e154268cd2e
bweinmann <bsweinmann@gmail.com>
2022-07-14 15:35:42 -0400

    test successful with hardcoded word, position tracking working, need to add condition for nonalpha characters

--- verbatim/src/App.js
+++ verbatim/src/App.js
@@ -1,42 +1,61 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': 'b1583183e4mshbac800953b86b23p16f609jsn7913d01b1356',
-//     'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
-//   }
-// };
-
-const inputLength = 5;
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'b1583183e4mshbac800953b86b23p16f609jsn7913d01b1356',
+    'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+  }
+};
 
 function App() {
 
-  // const [words, setWords] = useState([]);
-  const [inputs, setInput] = useState(Array(6).fill(null))
+  const [words, setWords] = useState('hello');
+  const [inputs, setInputs] = useState(Array(6).fill(null))
   const [currentInput, setCurrentInput] = useState('')
+  const [end, setEnd] = useState(false)
 
   useEffect(() => {
-    const handleInput = (event) => {
+    const handleInput = (e) => {
+      if (end) return;
+      //to handle user submission
+      if(e.key === 'Enter') {
+        if (currentInput.length !== 5) return;
+
+        const newInput = [...inputs];
+        newInput[inputs.findIndex(item => item == null)] = currentInput;
+        setInputs(newInput);
+        setCurrentInput('')
+
+        const correctAnswer = words === currentInput;
+        if (correctAnswer) setEnd(true);
+      }
+
+      if (e.key === 'Backspace') {
+        setCurrentInput(currentInput.slice(0, -1));
+        return;
+      }
+
+      if (currentInput.length >= 5) return;
       //will handle other non-alpha inputs later
       //may modify currentinput with a function, could be affected by the dependency array
-      setCurrentInput(currentInput + event.key)
+      setCurrentInput(currentInput + e.key)
     };
 
     window.addEventListener('keydown', handleInput);
     return () => window.removeEventListener('keydown', handleInput);
-  }, [currentInput]);
+  }, [currentInput, end, words]);
 
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const res = await fetch('https://random-words5.p.rapidapi.com/getRandom', options);
-  //     const random = await res.text();
-  //     setWords(random)
-  //   }
-  //   fetchData()
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch('https://random-words5.p.rapidapi.com/getRandom?wordLength=5', options);
+      const random = await res.text();
+      // setWords(random)
+    }
+    fetchData()
+  }, []);
 
   return (
     <div className="App">
@@ -46,9 +65,13 @@ function App() {
       <div className = "inputGrid">
         {inputs.map((input, i) => {
           //check if the index we are on is the correct one, ex. if box1 is null then we are on the first box
-          const inputChecker = i === inputs.findIndex(item => item == null);
+          const inputChecker = i === inputs.findIndex(val => val == null);
           return (
-            <Row input = {inputChecker ? currentInput : (input || '')}/>
+            <Row 
+            input = {inputChecker ? currentInput : (input || '')}
+            submission={!inputChecker && input != null}
+            words = {words}
+            />
           )
         })}
     </div>
@@ -56,13 +79,31 @@ function App() {
   );
 }
 
-const Row = ({input}) => {
+const Row = ({words, input, submission}) => {
   const boxes = [];
 
   //for loop is used instead of map because input could be null (empty string as the input)
-  for (let i = 0; i < inputLength; i++) {
+  for (let i = 0; i < 5; i++) {
     const char = input[i];
-    boxes.push(<div className="box">{char}</div>)
+    //if the user input is Enter
+    let className = 'box';
+    
+    if(submission) {
+      if (char === words[i]) {
+        className += ' right'
+      } else if (words.includes(char)) {
+        className += ' partial';
+      } else {
+        className += ' wrong';
+      }
+    }
+
+    boxes.push(
+    <div key={i} 
+    className={className}>
+      {char}
+      </div>
+      )
   }
 
   return <div className = "row">
