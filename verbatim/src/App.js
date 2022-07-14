e99da34d1a9f9251a9c708daa103927479692056
bweinmann <bsweinmann@gmail.com>
2022-07-14 13:33:39 -0400

    general styling completed, need to add keyframes below 450px

--- verbatim/src/App.js
+++ verbatim/src/App.js
@@ -1,32 +1,59 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 
-const options = {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': 'b1583183e4mshbac800953b86b23p16f609jsn7913d01b1356',
-    'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': 'b1583183e4mshbac800953b86b23p16f609jsn7913d01b1356',
+//     'X-RapidAPI-Host': 'random-words5.p.rapidapi.com'
+//   }
+// };
+
+const inputLength = 5;
 
 function App() {
 
-  const [words, setWords] = useState([]);
+  // const [words, setWords] = useState([]);
+  const [inputs, setInput] = useState(Array(6).fill(null))
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('https://random-words5.p.rapidapi.com/getRandom', options);
-      const random = await res.text();
-      setWords(random)
-    }
-    fetchData()
-  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch('https://random-words5.p.rapidapi.com/getRandom', options);
+  //     const random = await res.text();
+  //     setWords(random)
+  //   }
+  //   fetchData()
+  // }, []);
 
   return (
     <div className="App">
-      {words}
+
+       <div className = "header">
+        <h1>Verbatim</h1>
+       </div>
+      {
+        inputs.map(input => {
+          return (
+            <Row input = {input || ''}/>
+          )
+        })
+      }
     </div>
   );
+}
+
+const Row = ({input}) => {
+  const boxes = [];
+
+  //for loop is used instead of map because input could be null (empty string as the input)
+  for (let i = 0; i < inputLength; i++) {
+    const char = input[i];
+    boxes.push(<div className="box">{char}</div>)
+  }
+
+  return <div className = "row">
+    {boxes}
+  </div>
 }
 
 export default App;
